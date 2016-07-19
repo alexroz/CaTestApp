fc6437c7a29c4c513b5ad35de305f3cb88b25edb
alexroz <alex@rozycki.co.uk>
2016-07-19 09:41:18 +0100

    added retry blocks

---
+++ WebJobOne/Retry.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+using System;
+using System.Collections.Generic;
+
+namespace WebJobOne
+{
+    public static class Retry
+    {
+        public static void Run()
+        {
+            // Set up the retry strategy
+            string defaultRetryStrategyName = "myStrategy";
+            int retryCount = 10;
+            var retryInterval = TimeSpan.FromSeconds(10);
+            var strategy = new FixedInterval(defaultRetryStrategyName, retryCount, retryInterval);
+            var strategies = new List<RetryStrategy> { strategy };
+
+            // Create the retry manager
+            var manager = new RetryManager(strategies, defaultRetryStrategyName);
+
+            // Register the retry manager
+            RetryManager.SetDefault(manager);            
+
+            var policy = new RetryPolicy<SqlDatabaseTransientErrorDetectionStrategy>(3, TimeSpan.FromSeconds(5));
+
+            using (var conn = new ReliableSqlConnection("connectionString...", policy))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT TOP 1 Name FROM Users";
+                var result = cmd.ExecuteScalar();
+            }
+        }
+    }
+}
---
+++ WebJobOne/RetryStorage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using System;
+
+namespace WebJobOne
+{
+    public static class RetryStorage
+    {
+        public static void Retry()
+        {
+            string blobContainerName = "data";
+            var storageAccount = new CloudStorageAccount(new StorageCredentials("name", "secretkey"), true);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            blobClient.DefaultRequestOptions.RetryPolicy = new ExponentialRetry(TimeSpan.FromSeconds(2), 10);
+
+            CloudBlobContainer blobContainer = blobClient.GetContainerReference(blobContainerName);
+            bool containerExists = blobContainer.Exists();
+            
+            // ...
+        }
+    }
+}
